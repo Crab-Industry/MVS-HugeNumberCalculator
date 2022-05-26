4e6e1f8ad7c975cd88c86c8b927c919455b7cf80
Lucas <45604254+ItsLucas93@users.noreply.github.com>
2022-05-26 11:47:52 +0200

    Update ShowHugeNumber.c

--- IHMHugeNumber/ShowHugeNumber.c
+++ IHMHugeNumber/ShowHugeNumber.c
@@ -6,7 +6,7 @@
 #define SIZE_OF_EXPONENT_MARKUP 2
 #define EXPONENT_MARKUP " E"
 
-static char SignToChar(const Sign sign) {
+static char SignToChar (const Sign sign) {
     switch (sign) {
     case PLUS:
         return '+';
@@ -17,16 +17,16 @@ static char SignToChar(const Sign sign) {
     }
 }
 
-static char* HugeUnsignedIntToString(const HugeUnsignedInt* hugeUnsignedInt) {
-    const int stringLength = getHugeUnsignedIntLength(hugeUnsignedInt) + 1;
+static char* HugeUnsignedIntToString (const HugeUnsignedInt* hugeUnsignedInt) {
+    const int stringLength = getHugeUnsignedIntLength (hugeUnsignedInt) + 1;
     char* hugeUnsignedIntInString = NULL;
 
     if (hugeUnsignedInt != NULL) {
-        hugeUnsignedIntInString = malloc(stringLength * sizeof(HugeUnsignedInt));
+        hugeUnsignedIntInString = malloc (stringLength * sizeof (HugeUnsignedInt));
         if (hugeUnsignedIntInString != NULL) {
             Node* node = hugeUnsignedInt->start;;
-            for (int digitCounter = 0; (!isNodeEmpty(node)) && (digitCounter < stringLength); digitCounter++) {
-                hugeUnsignedIntInString[digitCounter] = digitToChar(node->digit);
+            for (int digitCounter = 0; (!isNodeEmpty (node)) && (digitCounter < stringLength); digitCounter++) {
+                hugeUnsignedIntInString[digitCounter] = digitToChar (node->digit);
                 node = node->next;
             }
             hugeUnsignedIntInString[stringLength - 1] = '\0';
@@ -36,59 +36,59 @@ static char* HugeUnsignedIntToString(const HugeUnsignedInt* hugeUnsignedInt) {
     return hugeUnsignedIntInString;
 }
 
-static char* HugeIntToString(const HugeInt* hugeInt) {
-    const int stringLength = getHugeIntLength(hugeInt) + 1;
+static char* HugeIntToString (const HugeInt* hugeInt) {
+    const int stringLength = getHugeIntLength (hugeInt) + 1;
     char* hugeIntInString = NULL;
 
     if (hugeInt != NULL) {
-        hugeIntInString = malloc(stringLength * sizeof(HugeInt));
+        hugeIntInString = malloc (stringLength * sizeof (HugeInt));
         if (hugeIntInString != NULL) {
-            char* absoluteValueInString = HugeUnsignedIntToString(hugeInt->absoluteValue);
-            hugeIntInString[0] = SignToChar(hugeInt->sign);
-            memcpy(hugeIntInString + 1, absoluteValueInString, stringLength);
-            free(absoluteValueInString);
+            char* absoluteValueInString = HugeUnsignedIntToString (hugeInt->absoluteValue);
+            hugeIntInString[0] = SignToChar (hugeInt->sign);
+            memcpy (hugeIntInString + 1, absoluteValueInString, stringLength);
+            free (absoluteValueInString);
         }
     }
 
     return hugeIntInString;
 }
 
-static char* HugeFloatToString(const HugeFloat* hugeFloat) {
-    const int stringLength = getHugeFloatLength(hugeFloat) + SIZE_OF_EXPONENT_MARKUP;
+static char* HugeFloatToString (const HugeFloat* hugeFloat) {
+    const int stringLength = getHugeFloatLength (hugeFloat) + SIZE_OF_EXPONENT_MARKUP;
     char* hugeFloatInString = NULL;
 
     if (hugeFloat != NULL) {
-        hugeFloatInString = malloc(stringLength * sizeof(HugeFloat));
+        hugeFloatInString = malloc (stringLength * sizeof (HugeFloat));
         if (hugeFloatInString != NULL) {
-            int significandLength = getHugeIntLength(hugeFloat->significand);
-            char* significandInString = HugeIntToString(hugeFloat->significand);
-            int exponentLength = getHugeIntLength(hugeFloat->exponent);
-            char* exponentInString = HugeIntToString(hugeFloat->exponent);
+            int significandLength = getHugeIntLength (hugeFloat->significand);
+            char* significandInString = HugeIntToString (hugeFloat->significand);
+            int exponentLength = getHugeIntLength (hugeFloat->exponent);
+            char* exponentInString = HugeIntToString (hugeFloat->exponent);
 
-            memcpy(hugeFloatInString, significandInString, significandLength);
-            free(significandInString);
-            memcpy(hugeFloatInString + significandLength, EXPONENT_MARKUP, SIZE_OF_EXPONENT_MARKUP);
-            memcpy(hugeFloatInString + significandLength + SIZE_OF_EXPONENT_MARKUP, exponentInString, exponentLength + 1);
-            free(exponentInString);
+            memcpy (hugeFloatInString, significandInString, significandLength);
+            free (significandInString);
+            memcpy (hugeFloatInString + significandLength, EXPONENT_MARKUP, SIZE_OF_EXPONENT_MARKUP);
+            memcpy (hugeFloatInString + significandLength + SIZE_OF_EXPONENT_MARKUP, exponentInString, exponentLength + 1);
+            free (exponentInString);
         }
     }
     return hugeFloatInString;
 }
 
-void printHugeUnsignedInt(const HugeUnsignedInt* hugeUnsignedInt) {
-    char* hugeUnsignedIntString = HugeUnsignedIntToString(hugeUnsignedInt);
-    printf("%s\n", hugeUnsignedIntString);
-    free(hugeUnsignedIntString);
+void printHugeUnsignedInt (const HugeUnsignedInt* hugeUnsignedInt) {
+    char* hugeUnsignedIntString = HugeUnsignedIntToString (hugeUnsignedInt);
+    printf ("%s\n", hugeUnsignedIntString);
+    free (hugeUnsignedIntString);
 }
 
-void printHugeInt(const HugeInt* hugeInt) {
-    char* hugeIntString = HugeIntToString(hugeInt);
-    printf("%s\n", hugeIntString);
-    free(hugeIntString);
+void printHugeInt (const HugeInt* hugeInt) {
+    char* hugeIntString = HugeIntToString (hugeInt);
+    printf ("%s\n", hugeIntString);
+    free (hugeIntString);
 }
 
-void printHugeFloat(const HugeFloat* hugeFloat) {
-    char* hugeFloatString = HugeFloatToString(hugeFloat);
-    printf("%s\n", hugeFloatString);
-    free(hugeFloatString);
+void printHugeFloat (const HugeFloat* hugeFloat) {
+    char* hugeFloatString = HugeFloatToString (hugeFloat);
+    printf ("%s\n", hugeFloatString);
+    free (hugeFloatString);
 }
