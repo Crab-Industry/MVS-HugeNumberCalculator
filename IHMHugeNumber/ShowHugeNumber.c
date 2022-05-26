a64c95d63b7858aada0db3bfb9ec1d5a93e2f85c
Lucas <45604254+ItsLucas93@users.noreply.github.com>
2022-05-26 11:12:33 +0200

    Update ShowHugeNumber.c

--- IHMHugeNumber/ShowHugeNumber.c
+++ IHMHugeNumber/ShowHugeNumber.c
@@ -1,12 +1,20 @@
 #include "ShowHugeNumber.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define SIZE_OF_EXPONENT_MARKUP 2
 #define EXPONENT_MARKUP " E"
 
 static char SignToChar (const Sign sign) {
-    return 0;
+    switch (sign) {
+        case PLUS:
+            return '+';
+        case MINUS:
+            return '-';
+        default:
+            return 'E';
+    }
 }
 
 static char* HugeUnsignedIntToString (const HugeUnsignedInt* hugeUnsignedInt) {
@@ -29,8 +37,22 @@ static char* HugeUnsignedIntToString (const HugeUnsignedInt* hugeUnsignedInt) {
 }
 
 static char* HugeIntToString (const HugeInt* hugeInt) {
-    return NULL;
+    const int stringLength = getHugeIntLength (hugeInt) + 1;
+    char* hugeIntInString = NULL;
+
+    if (hugeInt != NULL) {
+        hugeIntInString = malloc (stringLength * sizeof (HugeInt));
+        if (hugeIntInString != NULL) {
+            char* absoluteValueInString = HugeUnsignedIntToString (hugeInt->absoluteValue);
+            hugeIntInString[0] = SignToChar (hugeInt->sign);
+            memcpy (hugeIntInString + 1, absoluteValueInString, stringLength);
+            free (absoluteValueInString);
+        }
+    }
+
+    return hugeIntInString;
 }
+
 
 static char* HugeFloatToString (const HugeFloat* hugeFloat) {
     return NULL;
